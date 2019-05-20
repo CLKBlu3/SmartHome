eefa8ae6c03d5f54ec11ba4b40bf213ffb73ec5f
Marc Catrisse <marc@catrisse.net>
2019-05-20 23:29:25 +0200

    last retoques! actuadores

--- scripts/gauges/actuator.js
+++ scripts/gauges/actuator.js
@@ -1,6 +1,6 @@
 function generateActuatorSwitch(item,dataHome){ //generates el switch del actuador, devuelve el id para poder bindear con la base de datos
     let id = "actuator-switch-"+item.pin;
-    let actuatorSwitch = $("<div id='"+ id +"' class='switch'><label>Off<input type='checkbox'><span class='lever'></span>On</label></div>");
+    let actuatorSwitch = $("<div id='"+ id +"' class='switch'>Pin "+item.pin+": <label>Off<input type='checkbox'><span class='lever'></span>On</label></div>");
     $('#actuadors-container').append(actuatorSwitch);
     actuatorSwitch.on("click", function(event){
         event.preventDefault();
